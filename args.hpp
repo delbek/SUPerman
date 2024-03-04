a0867bfb97876262b2d234714124adc7213b475c
Olcay Oransoy <olcay.oransoy@gmail.com>
2024-03-04 16:30:04 +0300

    wrote to_flags method for args

--- args.hpp
+++ args.hpp
@@ -1,8 +1,9 @@
 #include "argparse.hpp"
+#include "flags.h"
 
 struct Args : argparse::Args {
     std::string &filename = arg("filename", "The path to the matrix file");
-    int &algo_id = kwarg("a,algo", "The algorithm to use").set_default(1);
+    int &algo_id = kwarg("p,algo", "The algorithm to use").set_default(0);
     int &threads = kwarg("t,threads", "The number of threads to use").set_default(16); 
     int &devices = kwarg("d,devices", "The number of devices to be used in a multi-GPU algorithm").set_default(2);
     int &trials = kwarg("x,trials", "The number of trials to run for an approximation algorithm").set_default(100000);
@@ -14,7 +15,7 @@ struct Args : argparse::Args {
     bool &cpu = flag("c,cpu", "Runs the algorithm on the CPU (if given with -g, runs a hybrid algorithm)");
     int &preprocessing = kwarg("r,preprocessing", "Preprocessing to be applied (1: SortOrder, 2: SkipOrder)").set_default(0); 
 
-    bool &grid = flag("i,grid", "Creates a grid graph and uses a sparse approximation algorithm"); 
+    bool &grid = flag("g,grid", "Creates a grid graph and uses a sparse approximation algorithm"); 
     int &gridm = kwarg("m,grid-m", "First dimension of the grid graph").set_default(36); 
     int &gridn = kwarg("n,grid-n", "Second dimension of the grid graph").set_default(36);
 
@@ -25,10 +26,55 @@ struct Args : argparse::Args {
 
     int &gpu_id = kwarg("gpu-id", "GPU id to run single GPU algorithm").set_default(0); 
     int &repetitions = kwarg("k", "The number of times to run the algorithm independently").set_default(1); 
-    int &cudaGridMultiplier = kwarg("e", "CUDA grid dimension multiplier").set_default(1);
-    bool &compression = flag("o", "Enable compression"); 
+    int &gpuGridMultiplier = kwarg("multiply-dim", "Multiplier for CUDA run-time chosen grid dimension for GPU algorithms").set_default(1);
+    bool &compression = flag("compress", "Enable compression"); 
 
-    double &scaleValue = kwarg("u", "Scale input matrix to value").set_default(1.0); 
+    double &scaleValue = kwarg("scale", "Scale input matrix to value").set_default(1.0); 
     int &scaleIntervals = kwarg("y,scale-intervals", "Scale intervals for a scaling approximation algorithm").set_default(4);
-    int &scaleTimes = kwarg("z,scale-times", "Number of times to scale for a scaling approximation algorithm").set_default(5); 
+    int &scaleCount = kwarg("z,scale-count", "Number of times to scale for a scaling approximation algorithm").set_default(5); 
+
+    bool &synchronizedGray = flag("j", "Unknown");
+
+    flags to_flags() {
+        flags f;
+
+        f.perman_algo = algo_id;
+        f.rep = repetitions;
+        f.threads = threads;
+        f.filename = filename.c_str();
+
+        f.dense = !sparse;
+        f.sparse = sparse;
+        f.exact = !approximation;
+        f.approximation = approximation;
+        f.binary_graph = binary || approximation;
+        f.number_of_times = trials;
+
+        f.gpu = gpu;
+        f.gpu_stated = gpu;
+        f.cpu = cpu;
+        f.gpu_num = devices;
+        f.device_id = gpu_id;
+
+        f.preprocessing = preprocessing;
+        f.compression = compression;
+
+        f.scale_times = scaleCount;
+        f.scale_intervals = scaleIntervals;
+        f.scaling_threshold = scaleValue;
+
+        f.grid_graph = grid;
+        f.gridm = gridm;
+        f.gridn = gridn;
+        f.grid_multip = gpuGridMultiplier;
+
+        f.calculation_half_precision = use32bitCalculation;
+        f.calculation_quad_precision = use128bitCalculation;
+        f.storage_half_precision = use32bitStorage;
+        f.storage_quad_precision = use128bitStorage;
+
+        f.synchronized_gray = synchronizedGray;
+
+        return f;
+    }
 };
